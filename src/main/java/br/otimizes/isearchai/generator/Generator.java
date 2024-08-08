00d7f85d23ac9a15b70f543fd0b7a25f520533e3
willianmarquesfreire <willian.freire@db1.com.br>
2024-08-08 12:12:47 -0300

    generation

---
+++ src/main/java/br/otimizes/isearchai/generator/Generator.java
@@ -0,0 +1,87 @@
+package br.otimizes.isearchai.generator;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class Generator {
+    public static void main(String[] args) {
+
+        try {
+            installDependencies();
+            extractZipFileFromResources("nautilus-framework-plugin.zip", "generated");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void installDependencies() throws IOException {
+        extractZipFileFromResources("jvalidation.zip", "generated");
+        mvnCleanInstall("generated/jvalidation");
+        extractZipFileFromResources("nautilus-framework.zip", "generated");
+        mvnCleanInstall("generated/nautilus-framework");
+    }
+
+    private static void mvnCleanInstall(String folder) {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.directory(new File(folder));
+        processBuilder.command("/home/wmfsystem/.sdkman/candidates/maven/3.9.8/bin/mvn", "clean", "install", "-DskipTests");
+
+        try {
+            Process process = processBuilder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            int exitCode = process.waitFor();
+            System.out.println("\nExited with code: " + exitCode);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void extractZipFileFromResources(String zipFileName, String outputDir) throws IOException {
+        // Get the input stream of the zip file from resources
+        try (InputStream inputStream = Generator.class.getClassLoader().getResourceAsStream(zipFileName);
+             ZipInputStream zipInputStream = new ZipInputStream(inputStream)) {
+
+            if (inputStream == null) {
+                throw new IOException("File not found: " + zipFileName);
+            }
+
+            // Create the output directory if it doesn't exist
+            Files.createDirectories(Paths.get(outputDir));
+
+            ZipEntry entry;
+            while ((entry = zipInputStream.getNextEntry()) != null) {
+                String filePath = outputDir + File.separator + entry.getName();
+
+                if (entry.isDirectory()) {
+                    Files.createDirectories(Paths.get(filePath));
+                } else {
+                    // Ensure parent directories exist
+                    Files.createDirectories(Paths.get(filePath).getParent());
+
+                    // Write file content
+                    try (FileOutputStream fos = new FileOutputStream(filePath)) {
+                        byte[] buffer = new byte[1024];
+                        int length;
+                        while ((length = zipInputStream.read(buffer)) > 0) {
+                            fos.write(buffer, 0, length);
+                        }
+                    }
+                }
+                zipInputStream.closeEntry();
+            }
+        }
+    }
+}
---
+++ src/main/java/br/otimizes/isearchai/generator/ObjectiveReplacer.java
@@ -0,0 +1,79 @@
+package br.otimizes.isearchai.generator;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+public class ObjectiveReplacer {
+    public static void main(String[] args) throws JsonProcessingException {
+        String jsonFile = readFileFromResources("nrp-generate.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode json = objectMapper.readTree(jsonFile);
+        JsonNode objectives = json.get("objectives");
+        for (JsonNode objective : objectives) {
+            String fileClass = readFileFromResources("nautilus-framework-plugin/src/main/java/org/nautilus/plugin/nrp/encoding/objective/Objective.java");
+            String className = objective.get("name").asText();
+            fileClass = fileClass.replace("$objective.name", className);
+            fileClass = fileClass.replace("$objective.type", objective.get("type").asText());
+            JsonNode incrementWithNode = objective.get("process").get("incrementWith");
+            String incrementWith = incrementWithNode.asText();
+            if (incrementWithNode.isTextual()) {
+                incrementWith = "instance.getSumOf" + incrementWith + "()";
+            }
+            fileClass = fileClass.replace("$objective.process.incrementWith", incrementWith);
+
+            fileClass = fileClass.replace("$objective.calculate.type", objective.get("calculate").get("type").asText());
+            String valueA = objective.get("calculate").get("a").get("value").asText();
+            if (!Objects.equals(valueA, "sum")) {
+                valueA = "instance.getSumOf" + valueA + "()";
+            }
+            fileClass = fileClass.replace("$objective.calculate.a", valueA);
+
+            String valueB = objective.get("calculate").get("b").get("value").asText();
+            if (!Objects.equals(valueB, "sum")) {
+                valueB = "instance.getSumOf" + valueB + "()";
+            }
+            fileClass = fileClass.replace("$objective.calculate.b", valueB);
+            writeFile("generated/nautilus-framework-plugin/src/main/java/org/nautilus/plugin/nrp/encoding/objective/" + className + ".java", fileClass);
+        }
+    }
+
+    public static void writeFile(String filePath, String content) {
+        File file = new File(filePath);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(content);
+            System.out.println("File written successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String readFileFromResources(String fileName) {
+        // Get the input stream of the file from resources
+        try (InputStream inputStream = ObjectiveReplacer.class.getClassLoader().getResourceAsStream(fileName);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+
+            if (inputStream == null) {
+                throw new IllegalArgumentException("File not found: " + fileName);
+            }
+
+            // Read the file line by line
+            String line;
+            String toReturn = "";
+            while ((line = reader.readLine()) != null) {
+                toReturn += line + "\n";
+                System.out.println(line);
+            }
+            return toReturn;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileName;
+    }
+}
